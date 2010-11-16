8eab43519d59268fa887e8153ccfebd35079a7e4
azserrata <azserrata@f5303fad-1203-8397-816c-ecd7e18a9f35>
2010-11-16 12:30:37 +0000

    FI

---
+++ file.lisp
@@ -0,0 +1,16 @@
+;;; cat file contents line by line
+;;     - filename: <self-describing>
+;;     - returns:  file content
+
+(defun s7-cat-file (filename) 
+  (if (probe-file filename)
+    (with-open-file (stream filename)
+      (loop for line = (read-line stream nil 'foo)
+          until (equal line 'foo)
+          do (print line)
+      )
+    )
+    nil ; filename not exists
+  )
+)
+
